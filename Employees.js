6b88f273cc590eb32de9795027c96fbf98a9cac2
Gary Hines <ghines1001@gmail.com>
2024-11-26 11:10:21 -0600

    Inital express app and test data

---
+++ Employees.js
@@ -0,0 +1,24 @@
+const employees = [
+    {
+        name:"Ram",
+        email:"ram@gmail.com",
+        age:23,
+    },
+    {
+        name:"Shyam",
+        email:"shyam@gmail.com",
+        age:65,
+    },
+    {
+        name:"Jon",
+        email:"jon@yahoo.com",
+        age:42,
+    },
+    {
+        name:"Francesca",
+        email:"francesca@gmail.com",
+        age:30,
+    },
+];
+
+module.exports = employees;
