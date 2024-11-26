c09d4acea4eb72933e9f9bd72d76b865f4ebe1dd
Gary Hines <ghines1001@gmail.com>
2024-11-26 15:52:19 -0600

    Added code for posting a new envelope

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ const express = require('express');
 const app = express();
 const envelopes = require('./Envelopes.js');
 
-// retrieve all employees
+app.use(express.json());
+
+// retrieve all envelopes
 app.get('/api/envelopes', (req, res) => {
     res.json(envelopes);
 })
 
-// retrieve single employee
+// retrieve single envelope
 app.get('/api/envelopes/:name', (req, res) => {   
     if (envelopes.some(envelope => envelope.name === req.params.name)) {
         res.json(envelopes.filter(envelope => envelope.name === req.params.name));
@@ -15,6 +17,22 @@ app.get('/api/envelopes/:name', (req, res) => {
         // 400 status = bad request
         res.status(400).json({msg: `Envelope ${req.params.name} does not exist`});
     }
+})
+
+// create envelope
+app.post('/api/envelopes', (req, res) => {
+    const newEnvelope = {
+        name: req.body.name,
+        allocatedAmount: req.body.allocatedAmount,
+        spentAmount: req.body.spentAmount,
+    }
+
+    if (!newEnvelope.name || !newEnvelope.allocatedAmount) {
+        res.status(400).json({msg: 'Please include both name and allocated amount'});
+    }
+
+    envelopes.push(newEnvelope);
+    res.json(envelopes);
 })
 
 const PORT = process.env.PORT || 4000;
