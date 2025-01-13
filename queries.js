74ec1a0e620f9de60d6dd8f97578a552d4705543
Gary Hines <ghines1001@gmail.com>
2025-01-13 14:05:18 -0600

    Fixed transactions code and tested with Postman

--- queries.js
+++ queries.js
@@ -1,88 +1,221 @@
+// const { get } = require("./routes/api/envelopes");
+
 /* import pg from 'pg'
    const { Pool } = pg
 OR */
-const Pool = require('pg').Pool
+const Pool = require("pg").Pool;
 const pool = new Pool({
-    user: 'budget',
-    password: '2025',
-    host: 'localhost',
-    database: 'personalbudget'
-})
+  user: "budget",
+  password: "2025",
+  host: "localhost",
+  database: "personalbudget",
+});
 
 const getEnvelopes = (req, res) => {
-    pool.query('SELECT * FROM envelopes ORDER BY id ASC', (error, results) => {
-        if (error) {
-            throw error
-        }
-        res.status(200).json(results.rows)
-    })
-}
+  pool.query("SELECT * FROM envelopes ORDER BY id ASC", (error, results) => {
+    if (error) {
+      throw error;
+    }
+    res.status(200).json(results.rows);
+  });
+};
 
 // retrieve single envelope
-router.getEnvelope('/:name', (req, res) => {
-    pool.query('SELECT * FROM envelopes WHERE name = $1', [req.params.name], (error, results) => {
-        if (error) {
-            throw error
-        }
-        res.status(200).json(results.rows)
-    })
-});
+const getEnvelope = (req, res) => {
+  pool.query(
+    "SELECT * FROM envelopes WHERE name = $1",
+    [req.params.name],
+    (error, results) => {
+      if (error) {
+        throw error;
+      }
+      res.status(200).json(results.rows);
+    });
+};
 
 // create envelope
-router.post('/', (req, res) => {
-    pool.query('INSERT INTO envelopes (name, allocatedAmount, spentAmount) VALUES ($1, $2, $3)', [req.body.name, req.body.allocatedAmount, req.body.spentAmount], (error, results) => {
-        if (error) {
-            throw error
-        }
-        res.status(201).send(`Envelope added with ID: ${result.insertId}`)
-    })
-});
-
+const createEnvelope = (req, res) => {
+  pool.query(
+    "INSERT INTO envelopes (name, balance) VALUES ($1, $2)",
+    [req.body.name, req.body.balance],
+    (error, results) => {
+      if (error) {
+        throw error;
+      }
+      res.status(201).send(`Envelope ${req.body.name} added with ${req.body.balance}`);
+    }
+  );
+};
 
 // update envelope list
-router.put('/:name', (req, res) => {
-    pool.query('UPDATE envelopes SET name = $1 WHERE name = $2', [req.body.name, req.params.name], (error, results) => {
-        if (error) {
-            throw error
-        }
-        res.status(200).send(`Envelope modified with ID: ${id}`)
-    }) 
-});
+const updateEnvelope = (req, res) => {
+  pool.query(
+    "UPDATE envelopes SET name = $1 WHERE name = $2",
+    [req.body.name, req.params.name],
+    (error, results) => {
+      if (error) {
+        throw error;
+      }
+      res.status(200).send(`Changed envelope ${req.params.name} to ${req.body.name}`);
+    }
+  );
+};
 
 // delete envelope
-router.delete('/:name', (req, res) => {
-    pool.query('DELETE FROM envelopes WHERE name = $1', [req.params.name], (error, results) => {
-        if (error) {
-            throw error
-        }
-        res.status(200).send(`Envelope deleted with ID: ${id}`)
-    })
-});
+const deleteEnvelope = (req, res) => {
+  pool.query(
+    "DELETE FROM envelopes WHERE name = $1",
+    [req.params.name],
+    (error, results) => {
+      if (error) {
+        throw error;
+      }
+      res.status(200).send(`Deleted envelope ${req.params.name}`);
+    }
+  );
+};
 
 // transfer from one envelope to another
-router.put('/transfer/:amount', (req, res) => {
-    // Parse and validate presence of amount, toAccount, and fromAccount
-    const amount = Number(req.params.amount);
-    if (!amount || isNaN(amount)) {
-        return res.status(400).send("Invalid or missing amount parameter.");
+//router.put("/transfer/:amount", (req, res) => {
+const transfer = (req, res) => {
+  // Parse and validate presence of amount, toAccount, and fromAccount
+  const amount = Number(req.params.amount);
+  if (!amount || isNaN(amount)) {
+    return res.status(400).send("Invalid or missing amount parameter.");
+  }
+
+  // Check if req.body contains the expected data
+  const { fromAccount, toAccount } = req.body;
+  if (!fromAccount || !toAccount) {
+    return res
+      .status(400)
+      .send("Missing 'fromAccount' or 'toAccount' in the request body.");
+  }
+
+  pool.query(
+    "UPDATE envelopes SET balance = balance + $1 WHERE name = $2",
+    [amount, toAccount],
+    (error, results) => {
+      if (error) {
+        throw error;
+      }
+      res.status(200).send(`Added ${amount} to ${toAccount}`);
     }
+  );
 
-    // Check if req.body contains the expected data
-    const { fromAccount, toAccount } = req.body;
-    if (!fromAccount || !toAccount) {
-        return res.status(400).send("Missing 'fromAccount' or 'toAccount' in the request body.");
+  pool.query(
+    "UPDATE envelopes SET balance = balance - $1 WHERE name = $2",
+    [amount, fromAccount],
+    (error, results) => {
+      if (error) {
+        throw error;
+      }
+      res.status(200).send(`${amount} from ${fromAccount} -> ${toAccount}`);
     }
+  );
+};
 
-    const fromAccountIndex = envelopes.findIndex(envelope => envelope.name === fromAccount);
-    const toAccountIndex = envelopes.findIndex(envelope => envelope.name === toAccount);
+// Write out new endpoints in order to create, read, update, and delete new transactions.
+// Create a new transaction
+const createTransaction = (req, res) => {
+    pool.query(
+        "INSERT INTO transactions (date, amount, payee, envelope_id) VALUES ($1, $2, $3, $4) RETURNING id",
+        [req.body.date, req.body.amount, req.body.payee, req.params.envelope_id],
+        (error, results) => {
+          if (error) {
+            throw error;
+          }
+          const transactionId = results.rows[0].id;
 
-    envelopes[fromAccountIndex].allocatedAmount -= amount;
-    envelopes[toAccountIndex].allocatedAmount += amount;
+          pool.query(
+            "UPDATE envelopes SET balance = balance - $1 WHERE id = $2",
+            [req.body.amount, req.params.envelope_id],
+            (error, results) => {
+              if (error) {
+                throw error;
+              }
+              res.status(201).send(`Transaction ${req.body.payee} for $${req.body.amount} added to envelope ${req.params.envelope_id}. Transaction ID: ${transactionId}. Subtracted ${req.body.amount} from envelope ${req.params.envelope_id}`);
+            }
+          );
+         }
+      );
+  };
 
-    res.send(`${amount} from ${fromAccount} -> ${toAccount}`);
+  // Read all transactions
+  const getTransactions = (req, res) => {
+    pool.query("SELECT * FROM transactions ORDER BY id ASC", (error, results) => {
+        if (error) {
+            res.status(500).send('Error executing query');
+            return;
+        }
+        res.status(200).json(results.rows);
+    });
+};
 
-});
+    // Read a single transaction
+    const getTransaction = (req, res) => {
+        pool.query(
+        "SELECT * FROM transactions WHERE id = $1",
+        [req.params.id],
+        (error, results) => {
+            if (error) {
+            throw error;
+            }
+            res.status(200).json(results.rows);
+        });
+    };
+
+    // Update a transaction by ID, by deleting transaction and creating a new one
+    const updateTransaction = (req, res) => {
+        pool.query(
+        "DELETE FROM transactions WHERE id = $1",
+        [req.params.id],
+        (error, results) => {
+            if (error) {
+            throw error;
+            }
+            pool.query(
+            "INSERT INTO transactions (date, amount, payee, envelope_id) VALUES ($1, $2, $3, $4) RETURNING id",
+            [req.body.date, req.body.amount, req.body.payee, req.body.envelope_id],
+            (error, results) => {
+                if (error) {
+                throw error;
+                }
+                const transactionId = results.rows[0].id;
+                res.status(201).send(`Deleted transaction with ID ${req.params.id}. Transaction ${transactionId} for $${req.body.amount} added to envelope ${req.body.envelope_id}`);
+            }
+            );
+        }
+        );
+    };
+
+    // Delete a transaction by ID
+    const deleteTransaction = (req, res) => {
+        pool.query(
+        "DELETE FROM transactions WHERE id = $1",
+        [req.params.id],
+        (error, results) => {
+            if (error) {
+            throw error;
+            }
+            res.status(200).send(`Deleted transaction with ID ${req.params.id}`);
+        }
+        );
+    };
+
+
+  
 
 module.exports = {
-    getEnvelopes
-}
+  getEnvelopes,
+  getEnvelope,
+  createEnvelope,
+  updateEnvelope,
+  deleteEnvelope,
+  transfer,
+  createTransaction,
+  getTransactions,
+  getTransaction,
+  updateTransaction,
+  deleteTransaction,
+};
--- routes/api/envelopes.js
+++ routes/api/envelopes.js
@@ -7,83 +7,34 @@ const db = require('../../queries');
 router.get('/', db.getEnvelopes);
 
 // retrieve single envelope
-router.get('/:name', (req, res) => {   
-    if (envelopes.some(envelope => envelope.name === req.params.name)) {
-        res.json(envelopes.filter(envelope => envelope.name === req.params.name));
-    } else {
-        // 400 status = bad request
-        res.status(400).json({msg: `Envelope ${req.params.name} does not exist`});
-    }
-});
+router.get('/:name', db.getEnvelope);
 
 // create envelope
-router.post('/', (req, res) => {
-    const newEnvelope = {
-        name: req.body.name,
-        allocatedAmount: req.body.allocatedAmount,
-        spentAmount: req.body.spentAmount,
-    }
-
-    if (!newEnvelope.name || !newEnvelope.allocatedAmount) {
-        res.status(400).json({msg: 'Please include both name and allocated amount'});
-    }
-
-    envelopes.push(newEnvelope);
-    res.json(envelopes);
-});
+router.post('/', db.createEnvelope);
 
 // update envelope list
-router.put('/:name', (req, res) => {
-    if (envelopes.some(envelope => envelope.name === req.params.name)) {
-        const updateEnvelope = req.body;
-        envelopes.forEach(envelope => {
-            if(envelope.name === req.params.name) {
-                envelope.name = updateEnvelope ? updateEnvelope.name : envelope.name;
-                res.json({msg: `Envelope updated`, envelope});
-            }
-        })
-    } else {
-        // 400 status = bad request
-        res.status(400).json({msg: `Envelope ${req.params.name} does not exist`});
-    }   
-});
+router.put('/:name', db.updateEnvelope);
 
 // delete envelope
-router.delete('/:name', (req, res) => {
-    if (envelopes.some(envelope => envelope.name === req.params.name)) {
-        res.json({
-            msg: `Envelope ${req.params.name} deleted`,
-            envelopes: envelopes.filter(envelope => envelope.name !== req.params.name)
-        });
-    } else {
-        // 400 status = bad request
-        res.status(400).json({msg: `Envelope ${req.params.name} does not exist`});
-    }   
-});
+router.delete('/:name', db.deleteEnvelope);
 
 // transfer from one envelope to another
-router.put('/transfer/:amount', (req, res) => {
-    // Parse and validate presence of amount, toAccount, and fromAccount
-    const amount = Number(req.params.amount);
-    if (!amount || isNaN(amount)) {
-        return res.status(400).send("Invalid or missing amount parameter.");
-    }
+router.put('/transfer/:amount', db.transfer);
 
-    // Check if req.body contains the expected data
-    const { fromAccount, toAccount } = req.body;
-    if (!fromAccount || !toAccount) {
-        return res.status(400).send("Missing 'fromAccount' or 'toAccount' in the request body.");
-    }
+// create transaction
+router.post('/transaction/:envelope_id', db.createTransaction);
 
-    const fromAccountIndex = envelopes.findIndex(envelope => envelope.name === fromAccount);
-    const toAccountIndex = envelopes.findIndex(envelope => envelope.name === toAccount);
+// get all transactions
+router.get('/transactions/all', db.getTransactions);
 
-    envelopes[fromAccountIndex].allocatedAmount -= amount;
-    envelopes[toAccountIndex].allocatedAmount += amount;
+// get a single transaction by id   
+router.get('/transaction/:id', db.getTransaction);
 
-    res.send(`${amount} from ${fromAccount} -> ${toAccount}`);
+// update transaction (deletes and inserts new transaction)
+router.put('/transaction/update/:id', db.updateTransaction);
 
-});
+// delete transaction   
+router.delete('/transaction/delete/:id', db.deleteTransaction);
 
 
 module.exports = router;
